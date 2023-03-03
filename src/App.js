9c0319176a1a108ad0035aaf1ee1bb2af998aa88
subhashkhatri <subhash.chandur66@gmail.com>
2023-03-03 20:10:28 +0500

    Sub

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Subhash Chandur This is my First React App</h1>
     </div>
   );
 }
